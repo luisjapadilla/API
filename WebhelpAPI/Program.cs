f116355584e9a2a21bbd4e9611ec6e57a5faf9fe
Luis Jesus Augusto Padilla Gil <jesus.padilla@onelinkbpo.com>
2022-09-29 00:15:35 -0500

    unit Tests

--- WebhelpAPI/Program.cs
+++ WebhelpAPI/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddCors(options =>
        {
            builder.WithOrigins("http://localhost:4200")
            .AllowAnyMethod()
+           .AllowAnyHeader()
            .AllowAnyMethod();
        });
 });
